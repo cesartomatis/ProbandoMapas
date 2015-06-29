7ea186ba8e0bf71f279b12b3a4de8ceb005ec1fc
AbelAlejandro <abel.alejandro@ymail.com>
2015-06-29 17:24:48 -0300

    Itinerary Layout on XAML

---
+++ ProbandoMapas/ProbandoMapas/ViewModel/VMListadoItinerarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+using ProbandoMapas.Model;
+
+namespace ProbandoMapas.ViewModel
+{
+    public class VMListadoItinerarios: INotifyPropertyChanged
+    {
+        public string ordinalNumber { get; set; }
+        public string ClientName { get; set; }
+        public string ClientAdress { get; set; }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
