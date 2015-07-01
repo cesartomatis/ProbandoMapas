1c0b66df5d6bea19cde1069a2d948c74611b26c7
AbelAlejandro <abel.alejandro@ymail.com>
2015-07-01 12:20:33 -0300

    Added Itinerary detail View
    It's the view that's supposed to show the map with the suggested
    itinerary. This functionality will be most likely provided by the
    salesforce API but I have nothing better to do, soooo

--- ProbandoMapas/ProbandoMapas/Model/Itinerary.cs
+++ ProbandoMapas/ProbandoMapas/Model/Itinerary.cs
@@ -5,8 +5,8 @@ namespace ProbandoMapas.Model
     public class Itinerary
     {
         public string Description { get; set; }
-        public string OriginAndress { get; set; }
-        public string FinalAndress { get; set; }
+        public string OriginAdress { get; set; }
+        public string FinalAdress { get; set; }
         public double ItineraryMiles { get; set; }
         public DateTime OriginTime { get; set; }
         public DateTime FinalTime { get; set; }
