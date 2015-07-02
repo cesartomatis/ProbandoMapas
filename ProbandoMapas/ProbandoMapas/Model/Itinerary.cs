b8da0ca4301c487a849f5c5e88db7d084422f1a6
paul33868 <gil.a.pablo@gmail.com>
2015-07-02 09:32:14 -0300

    Another commit

--- ProbandoMapas/ProbandoMapas/Model/Itinerary.cs
+++ ProbandoMapas/ProbandoMapas/Model/Itinerary.cs
@@ -5,11 +5,12 @@ namespace ProbandoMapas.Model
     public class Itinerary
     {
         public string Description { get; set; }
-        public string OriginAndress { get; set; }
-        public string FinalAndress { get; set; }
+        public string OriginAdress { get; set; }
+        public string FinalAdress { get; set; }
         public double ItineraryMiles { get; set; }
         public DateTime OriginTime { get; set; }
         public DateTime FinalTime { get; set; }
         public DateTime ItineraryDay { get; set; }
+        public int IdItinerario { get; set; }
     }
 }
