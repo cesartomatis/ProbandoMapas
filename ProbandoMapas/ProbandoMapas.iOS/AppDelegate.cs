7d2d8ecdb9bf7aa1bd97b9e51899a796b63a8b76
AbelAlejandro <abel.alejandro@ymail.com>
2015-07-08 12:06:33 -0300

    Agregado lo de la Geodesia pero no esta implementado en los itinerarios de la lista

--- ProbandoMapas/ProbandoMapas.iOS/AppDelegate.cs
+++ ProbandoMapas/ProbandoMapas.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace ProbandoMapas.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            global::Xam.Plugin.MapExtend.iOSUnified.MapExtendRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
